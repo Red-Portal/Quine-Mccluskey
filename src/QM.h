aff395dbe424259c1cc650c99931180da3e28b91
RedPortal <msca8h@naver.com>
2017-05-05 14:12:18 +0900

    backup during implementation of findImplicants backup
    memo: the second bool in the pair is supposed to be true at first
          if the term is not an implicant, then it is converted to false

--- src/QM.h
+++ src/QM.h
@@ -18,6 +18,8 @@ namespace QM
 		std::vector<std::bitset<_Size>> _minTerms;
 		std::vector<std::bitset<_Size>> _dTerms;
 
+        auto findImplicants(std::vector<std::reference_wrapper<std::bitset<_Size>>> const&)
+		-> std::vector<std::pair<std::bitset<_Size>, bool>>;
 
     public:
         explicit Reducer(std::vector<int> const& minTerms,
@@ -38,9 +40,9 @@ QM::Reducer<_Size>::Reducer(std::vector<int> const& minTerms_,
 {
 	_minTerms.reserve(minTerms_.size());
 	_dTerms.reserve(dTerms_.size());
+
 	for (auto const& it : minTerms_)
 		_minTerms.emplace_back(std::bitset<_Size>(static_cast<unsigned long>(it)));
-
 	for (auto const& it : dTerms_)
 		_dTerms.emplace_back(std::bitset<_Size>(static_cast<unsigned long>(it)));
 
@@ -59,7 +61,6 @@ QM::Reducer<_Size>::Reducer(std::array<int, _Size> const& minTerms_,
 
 	for (auto const& it : minTerms_)
 		_minTerms.emplace_back(std::bitset<_Size>(static_cast<unsigned long>(it)));
-
 	for (auto const& it : dTerms_)
 		_dTerms.emplace_back(std::bitset<_Size>(static_cast<unsigned long>(it)));
 
@@ -71,7 +72,7 @@ QM::Reducer<_Size>::Reducer(std::array<int, _Size> const& minTerms_,
 template<size_t _Size>
 void QM::Reducer<_Size>::reduce()
 {
-	std::array<std::vector<std::reference_wrapper<std::bitset<_Size>>>, _Size + 1> bitsByCount;
+	auto bitsByCount = std::array<std::vector<std::reference_wrapper<std::bitset<_Size>>>, _Size + 1>() ;
 
 	// seperating the bitsets by the number of 1s
 	for(auto& it : _minTerms)
@@ -79,7 +80,14 @@ void QM::Reducer<_Size>::reduce()
     for(auto& it : _dTerms)
         bitsByCount[it.count()].push_back(std::ref(it));
 
+	for(auto& it : bitsByCount)
+		findImplicants(it);
+}
 
-
+template<size_t _Size>
+std::vector<std::pair<std::bitset<_Size>, bool>>
+QM::Reducer<_Size>::findImplicants(std::vector<std::reference_wrapper<std::bitset<_Size>>> const&)
+{
+	return std::vector<std::pair<std::bitset<_Size>, bool>>();
 }
 #endif
