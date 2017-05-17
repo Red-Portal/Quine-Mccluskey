c156592423247d5911347f9b53c9467ee06bc33b
Red-Portal <msca8h@naver.com>
2017-05-17 23:11:51 +0900

    reformated Term.hpp to a header only

--- src/Term.hpp
+++ src/Term.hpp
@@ -2,54 +2,101 @@
 #define TERM_HPP
 
 #include <vector>
+#include <memory>
 #include <cstdlib>
+
+#include <iostream>
 
 namespace QM
 {
+	template<typename BitArray>
 	class Term
 	{
-		using BitArray = unsigned long;
- 
 	private:
 		int _numberOfOne;
 		bool _checked;
-		size_t const _size;
+		size_t _size;
 		BitArray _bitArray;
 		BitArray _xMask;
 
 	public:
-		Term() = default;
-		Term(size_t size, unsigned long term);
-		Term(size_t size, unsigned long term, BitArray mask);
+		inline Term() = default;
+		inline Term(Term<BitArray>&& other) = default;
+		inline Term(Term<BitArray> const& other) = default;
+		inline Term(size_t size, BitArray term);
+		inline Term(size_t size, BitArray term, BitArray mask);
+		inline Term<BitArray>& operator=(Term const& other) = default;
+		inline Term<BitArray>& operator=(Term&& other) = default;
 		~Term() = default;
 
-		std::pair<Term, bool> compareIfImplicant(Term& other);
-
-		inline void operator=(Term const& other)
-		{
-		    _numberOfOne = other._numberOfOne;
-			_checked = other._checked;
-			_bitArray = other._bitArray;
-			_xMask = other._xMask;
-		}
-
-		inline void operator=(Term&& other)
-		{
-		    _numberOfOne = other._numberOfOne;
-			_checked = other._checked;
-			_bitArray = other._bitArray;
-			_xMask = other._xMask;
-		}
-
-		inline int getNumberOfOnes() const
-		{
-			return _numberOfOne;
-		}
-
-		inline bool isChecked() const
-		{
-			return _checked;
-		}
+		std::unique_ptr<Term<BitArray>> compareIfImplicant(Term& other);
+		inline int getNumberOfOnes() const;
+		inline bool isChecked() const;
 	};
+
+
+
+	template<typename BitArray>
+	inline Term<BitArray>::Term(size_t size, BitArray term)
+		:_size(size),
+		 _bitArray(term),
+		 _numberOfOne(0),
+		 _checked(false),
+		 _xMask(0)
+	{
+		BitArray end = 1 << size;
+		for(BitArray it = 1; it != end; it <<= 1)
+		{
+			if(_bitArray & it)
+				++_numberOfOne;
+		}
+	}
+
+
+	template<typename BitArray>
+	inline Term<BitArray>::Term(size_t size, BitArray term, BitArray mask)
+		:_size(size),
+		 _bitArray(term),
+		 _numberOfOne(0),
+		 _checked(false),
+		 _xMask(mask)
+	{
+		BitArray end = 1 << size;
+		for(BitArray it = 1; it != end; it <<= 1)
+		{
+			if(_bitArray & it)
+				++_numberOfOne;
+		}
+	}
+
+	template<typename BitArray>
+	std::unique_ptr<Term<BitArray>> Term<BitArray>::compareIfImplicant(Term &other)
+	{
+		auto result = _bitArray ^ other._bitArray; //XOR operation
+		// if the result has only one '1' then it's an implicant
+
+		if(result > 0 && result % 2 == 0)
+		{
+			
+		}
+		else
+			return nullptr;
+	}
+
+	template<typename BitArray>
+	inline int Term<BitArray>::getNumberOfOnes() const
+	{
+		return _numberOfOne;
+	}
+
+
+	template<typename BitArray>
+	inline bool Term<BitArray>::isChecked() const
+	{
+		return _checked;
+	}
 }
+
+
+
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,11 @@
 
 int main( int argc, char** argv)
 {
-    int const termNum = 4;
     //std::vector<int> minTerms = {4, 8, 10, 11, 12, 15};
     //std::vector<int> dTerms = {9, 14};
 
-	auto term = QM::Term(4, 3);
+	auto term = QM::Term<uint64_t>(4, 3);
+
 	if(term.getNumberOfOnes() == 2)
 		std::cout << "Passed computing numberOfOnes Test";
 
