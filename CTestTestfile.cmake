7a1efbd7701056d7078e25d2a735eedb2495531d
Ray Kim <msca8h@naver.com>
2017-05-22 02:30:32 +0900

    Delete CTestTestfile.cmake

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/msca8h/projects/Quine-Mccluskey
-# Build directory: /home/msca8h/projects/Quine-Mccluskey
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(QuineMccluskeyTest "QuineMccluskeyTest")
-subdirs("googletest/googletest")
