58342d49fd028cc8e8dc959c70132f0a55984504
Red-Portal <msca8h@naver.com>
2017-05-22 01:58:40 +0900

    added gtest for unit testing

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/msca8h/projects/Quine-Mccluskey
+# Build directory: /home/msca8h/projects/Quine-Mccluskey
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(QuineMccluskeyTest "QuineMccluskeyTest")
+subdirs("googletest/googletest")
