0701eae9511aebb08d738015e53e36ff8dee2546
Red-Portal <msca8h@naver.com>
2017-06-06 22:17:32 +0900

    formatted tabs to spaces

--- src/QM.hpp
+++ src/QM.hpp
@@ -16,267 +16,267 @@
 
 namespace QM
 {
-	template<typename BitArray>
-	class Reducer
-	{
-	private:
-		using TermVector = std::vector<Term<BitArray>>;
+    template<typename BitArray>
+    class Reducer
+    {
+    private:
+        using TermVector = std::vector<Term<BitArray>>;
 
-		size_t const _inputNum;
+        size_t const _inputNum;
 
-		inline auto computeByNumberOfSetBits(
-			std::unordered_map<BitArray, TermVector>& map) const;
+        inline auto computeByNumberOfSetBits(
+            std::unordered_map<BitArray, TermVector>& map) const;
 
-		inline auto computeImplicant(TermVector& first,
-									 TermVector& second) const;
+        inline auto computeImplicant(TermVector& first,
+                                     TermVector& second) const;
 
-		inline auto getImplicants(
-			std::unordered_map<BitArray, TermVector> const&) const;
+        inline auto getImplicants(
+            std::unordered_map<BitArray, TermVector> const&) const;
 
-		inline auto reduce(TermVector const& mTerms, TermVector const& dTerms) const;
+        inline auto reduce(TermVector const& mTerms, TermVector const& dTerms) const;
 
-		inline auto select(TermVector const& implicants, TermVector const& mTerms) const;
+        inline auto select(TermVector const& implicants, TermVector const& mTerms) const;
 
-		inline void setResult(TermVector const& implicants);
+        inline void setResult(TermVector const& implicants);
 
-		std::vector<std::vector<int>> _result;
+        std::vector<std::vector<int>> _result;
 
-	public:
-		template<typename T>
-		inline explicit Reducer(int inputNum,
-								std::vector<T> const& minTerms,
-								std::vector<T> const& dTerms);
-		template<typename T>
-		inline explicit Reducer(int inputNum,
-								std::initializer_list<T> const& minTerms,
-								std::initializer_list<T> const& dTerms);
+    public:
+        template<typename T>
+        inline explicit Reducer(int inputNum,
+                                std::vector<T> const& minTerms,
+                                std::vector<T> const& dTerms);
+        template<typename T>
+        inline explicit Reducer(int inputNum,
+                                std::initializer_list<T> const& minTerms,
+                                std::initializer_list<T> const& dTerms);
 
-		~Reducer() = default;
-	};
+        ~Reducer() = default;
+    };
 
 
-	template<typename BitArray>
-	template<typename T>
-	Reducer<BitArray>::Reducer(int inputNum,
-							   std::vector<T> const& minTerms,
-							   std::vector<T> const& dTerms)
-		:_inputNum(inputNum)
-	{
-		TermVector mTerms_;
-		TermVector dTerms_;
+    template<typename BitArray>
+    template<typename T>
+    Reducer<BitArray>::Reducer(int inputNum,
+                               std::vector<T> const& minTerms,
+                               std::vector<T> const& dTerms)
+        :_inputNum(inputNum)
+    {
+        TermVector mTerms_;
+        TermVector dTerms_;
 
-		mTerms_.reserve(minTerms.size());
-		dTerms_.reserve(dTerms.size()); 
+        mTerms_.reserve(minTerms.size());
+        dTerms_.reserve(dTerms.size()); 
 
-		for (auto it : minTerms)
-			mTerms_.emplace_back(Term<BitArray>(inputNum, it));
-		for (auto it : dTerms)
-			dTerms_.emplace_back(Term<BitArray>(inputNum, it));
+        for (auto it : minTerms)
+            mTerms_.emplace_back(Term<BitArray>(inputNum, it));
+        for (auto it : dTerms)
+            dTerms_.emplace_back(Term<BitArray>(inputNum, it));
 
-		auto implicants = reduce(mTerms_, dTerms_);
-		auto primeImpl = select(implicants, mTerms_);
-		setResult(primeImpl);
-	}
+        auto implicants = reduce(mTerms_, dTerms_);
+        auto primeImpl = select(implicants, mTerms_);
+        setResult(primeImpl);
+    }
 
-	template<typename BitArray>
-	template<typename T>
-	Reducer<BitArray>::Reducer(int inputNum,
-							   std::initializer_list<T> const& minTerms,
-							   std::initializer_list<T> const& dTerms)
-		: _inputNum(inputNum)
-	{
-		TermVector mTerms_;
-		TermVector dTerms_;
+    template<typename BitArray>
+    template<typename T>
+    Reducer<BitArray>::Reducer(int inputNum,
+                               std::initializer_list<T> const& minTerms,
+                               std::initializer_list<T> const& dTerms)
+        : _inputNum(inputNum)
+    {
+        TermVector mTerms_;
+        TermVector dTerms_;
 
-		mTerms_.reserve(minTerms.size());
-		dTerms_.reserve(dTerms.size());
+        mTerms_.reserve(minTerms.size());
+        dTerms_.reserve(dTerms.size());
 
-		for (auto it : minTerms)
-			mTerms_.emplace_back(Term<BitArray>(inputNum, it));
-		for (auto it : dTerms)
-			dTerms_.emplace_back(Term<BitArray>(inputNum, it));
+        for (auto it : minTerms)
+            mTerms_.emplace_back(Term<BitArray>(inputNum, it));
+        for (auto it : dTerms)
+            dTerms_.emplace_back(Term<BitArray>(inputNum, it));
 
-		auto implicants = reduce(mTerms_, dTerms_);
-		auto primeImpl = select(implicants, mTerms_);
-		setResult(primeImpl);
-	}
+        auto implicants = reduce(mTerms_, dTerms_);
+        auto primeImpl = select(implicants, mTerms_);
+        setResult(primeImpl);
+    }
 
-	template<typename BitArray>
-	auto Reducer<BitArray>::reduce(TermVector const& mTerms,
-								   TermVector const& dTerms) const
-	{
-		std::vector<std::unordered_map<BitArray, TermVector>> byImplicantSize;
-		TermVector result;
-		result.reserve(mTerms.size() + dTerms.size());
+    template<typename BitArray>
+    auto Reducer<BitArray>::reduce(TermVector const& mTerms,
+                                   TermVector const& dTerms) const
+    {
+        std::vector<std::unordered_map<BitArray, TermVector>> byImplicantSize;
+        TermVector result;
+        result.reserve(mTerms.size() + dTerms.size());
 
-		// seperating the bitsets by the number of 1s
-		byImplicantSize.emplace_back(std::unordered_map<BitArray, TermVector>());
-		auto& sizeZero = byImplicantSize.front();
+        // seperating the bitsets by the number of 1s
+        byImplicantSize.emplace_back(std::unordered_map<BitArray, TermVector>());
+        auto& sizeZero = byImplicantSize.front();
 
-		for(auto& it : mTerms)
-			sizeZero[it.getSetBitNum()].push_back(it);
-		for(auto& it : dTerms)
-			sizeZero[it.getSetBitNum()].push_back(it);
+        for(auto& it : mTerms)
+            sizeZero[it.getSetBitNum()].push_back(it);
+        for(auto& it : dTerms)
+            sizeZero[it.getSetBitNum()].push_back(it);
 
-		for(BitArray idx = 0; idx < byImplicantSize.size(); ++idx)
-		{
-			auto implicant = computeByNumberOfSetBits(byImplicantSize[idx]);
+        for(BitArray idx = 0; idx < byImplicantSize.size(); ++idx)
+        {
+            auto implicant = computeByNumberOfSetBits(byImplicantSize[idx]);
 
-			if(implicant.empty())
-				continue;
+            if(implicant.empty())
+                continue;
 
-			byImplicantSize.push_back(std::move(implicant));
-		} 
+            byImplicantSize.push_back(std::move(implicant));
+        } 
 
-		for(auto& i : byImplicantSize)
-		{
-			auto realImp = getImplicants(i);
-			result.insert(result.end(),
-						  std::make_move_iterator(realImp.begin()),
-						  std::make_move_iterator(realImp.end()));
-		}
+        for(auto& i : byImplicantSize)
+        {
+            auto realImp = getImplicants(i);
+            result.insert(result.end(),
+                          std::make_move_iterator(realImp.begin()),
+                          std::make_move_iterator(realImp.end()));
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	template<typename BitArray>
-	auto Reducer<BitArray>::computeByNumberOfSetBits(std::unordered_map<BitArray, TermVector>& map) const
-	{
-		std::unordered_map<BitArray, TermVector> result;
+    template<typename BitArray>
+    auto Reducer<BitArray>::computeByNumberOfSetBits(std::unordered_map<BitArray, TermVector>& map) const
+    {
+        std::unordered_map<BitArray, TermVector> result;
 
-		for(BitArray i = 0; i < _inputNum; ++i)
-		{
-			auto first = map.find(i);
-			if(first == map.end())
-				continue;
+        for(BitArray i = 0; i < _inputNum; ++i)
+        {
+            auto first = map.find(i);
+            if(first == map.end())
+                continue;
 
-			auto second = map.find(i+1);
-			if(second == map.end())
-				continue;
+            auto second = map.find(i+1);
+            if(second == map.end())
+                continue;
 
-			result[i] = computeImplicant(first->second,
-										 second->second);
-		}
+            result[i] = computeImplicant(first->second,
+                                         second->second);
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	template<typename BitArray>
-	auto Reducer<BitArray>::computeImplicant(TermVector& first,
-											 TermVector& second) const
-	{
-		std::set<Term<BitArray>> set;
+    template<typename BitArray>
+    auto Reducer<BitArray>::computeImplicant(TermVector& first,
+                                             TermVector& second) const
+    {
+        std::set<Term<BitArray>> set;
 
-		for(auto i = first.begin(); i != first.end(); ++i)
-		{
-			for(auto j = second.begin(); j != second.end(); ++j)
-			{
-				if(i->isGreyAdjacent(*j))
-				{
-					auto ret = i->getGroupedTerm(*j);
-					set.insert(ret);
-				}
-			}
-		}
-		return TermVector(set.begin(), set.end());
-	}
+        for(auto i = first.begin(); i != first.end(); ++i)
+        {
+            for(auto j = second.begin(); j != second.end(); ++j)
+            {
+                if(i->isGreyAdjacent(*j))
+                {
+                    auto ret = i->getGroupedTerm(*j);
+                    set.insert(ret);
+                }
+            }
+        }
+        return TermVector(set.begin(), set.end());
+    }
 
-	template<typename BitArray>
-	auto Reducer<BitArray>::getImplicants(std::unordered_map<BitArray, TermVector> const& map) const
-	{
-		TermVector result;
-		for(auto& i : map)
-		{
-			if(i.second.size() == 0)
-				continue;
-			
-			for(auto& j : i.second)
-			{
-				if(j.isChecked())
-					result.push_back(std::move(j));
-			}
-		}
+    template<typename BitArray>
+    auto Reducer<BitArray>::getImplicants(std::unordered_map<BitArray, TermVector> const& map) const
+    {
+        TermVector result;
+        for(auto& i : map)
+        {
+            if(i.second.size() == 0)
+                continue;
+            
+            for(auto& j : i.second)
+            {
+                if(j.isChecked())
+                    result.push_back(std::move(j));
+            }
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	template<typename BitArray>
-	auto Reducer<BitArray>::select(TermVector const& implicants,
-								   TermVector const& mTerms) const
-	{
-		std::unordered_map<BitArray, std::vector<Term<BitArray> const*>> chart;
-		std::unordered_map<Term<BitArray> const*, bool> implicantsCheckList; 
-		std::set<Term<BitArray>> result;
+    template<typename BitArray>
+    auto Reducer<BitArray>::select(TermVector const& implicants,
+                                   TermVector const& mTerms) const
+    {
+        std::unordered_map<BitArray, std::vector<Term<BitArray> const*>> chart;
+        std::unordered_map<Term<BitArray> const*, bool> implicantsCheckList; 
+        std::set<Term<BitArray>> result;
 
-		for(auto& i : implicants)
-		{
-			implicantsCheckList[&i] = false;
+        for(auto& i : implicants)
+        {
+            implicantsCheckList[&i] = false;
 
-			for(auto& j : i._minTerm)
-			{
-				chart[j].push_back(&i);
-			}
-		}
+            for(auto& j : i._minTerm)
+            {
+                chart[j].push_back(&i);
+            }
+        }
 
-		// selection of prime implicants
-		for(auto& i : chart)
-		{
-			if(i.second.size() == 1)
-			{
-				result.emplace(*(i.second.front()));
-				implicantsCheckList[i.second.front()] = true;
-			}
-		}
+        // selection of prime implicants
+        for(auto& i : chart)
+        {
+            if(i.second.size() == 1)
+            {
+                result.emplace(*(i.second.front()));
+                implicantsCheckList[i.second.front()] = true;
+            }
+        }
 
-		// selection of non-prime implicants
-		for(auto& i : chart)
-		{
-			if(i.second.size() > 1)
-			{
-				// j is a pointer
-				for(auto j : i.second)
-				{
-					if(result.find(*j) != result.end())
-					{
-						result.emplace(*j);
-						goto OUT;
-					}
-				}
+        // selection of non-prime implicants
+        for(auto& i : chart)
+        {
+            if(i.second.size() > 1)
+            {
+                // j is a pointer
+                for(auto j : i.second)
+                {
+                    if(result.find(*j) != result.end())
+                    {
+                        result.emplace(*j);
+                        goto OUT;
+                    }
+                }
 
-				for(auto j : i.second)
-				{
-					
-					if(implicantsCheckList[j] != true)
-					{
-						result.emplace(*j);
-						break;
-					}
-				}
-			}
+                for(auto j : i.second)
+                {
+                    
+                    if(implicantsCheckList[j] != true)
+                    {
+                        result.emplace(*j);
+                        break;
+                    }
+                }
+            }
 
-		OUT:
-			;
-		}
+        OUT:
+            ;
+        }
 
-		return TermVector(result.begin(), result.end());
-	}
+        return TermVector(result.begin(), result.end());
+    }
 
-	template<typename BitArray>
-	void Reducer<BitArray>::setResult(TermVector const& implicants)
-	{
-		_result.reserve(implicants.size());
-		for(auto& i : implicants)
-		{
-			_result.emplace_back();
+    template<typename BitArray>
+    void Reducer<BitArray>::setResult(TermVector const& implicants)
+    {
+        _result.reserve(implicants.size());
+        for(auto& i : implicants)
+        {
+            _result.emplace_back();
 
-			auto equation = i.getEquation();
+            auto equation = i.getEquation();
 
-			auto& back = _result.back();
-			back.reserve(equation.size());
-			back.insert(back.end(),
-						std::make_move_iterator(equation.begin()),
-						std::make_move_iterator(equation.end()));
-		}
-	}
+            auto& back = _result.back();
+            back.reserve(equation.size());
+            back.insert(back.end(),
+                        std::make_move_iterator(equation.begin()),
+                        std::make_move_iterator(equation.end()));
+        }
+    }
 }
 #endif
--- src/Term.hpp
+++ src/Term.hpp
@@ -9,165 +9,165 @@
 
 namespace QM
 {
-	template<typename BitArray>
-	class Term
-	{
-	private:
-		int _numberOfOne;
-		bool _checked;
-		size_t _size;
-		BitArray _bitArray;
-		BitArray _xMask;
-		BitArray _result;
-		std::vector<BitArray> _minTerm;
+    template<typename BitArray>
+    class Term
+    {
+    private:
+        int _numberOfOne;
+        bool _checked;
+        size_t _size;
+        BitArray _bitArray;
+        BitArray _xMask;
+        BitArray _result;
+        std::vector<BitArray> _minTerm;
 
-	public:
-		inline Term(size_t size, BitArray term);
-		inline Term(Term<BitArray> const& first,
-					Term<BitArray> const& second,
-					BitArray mask);
-		inline Term(Term const&) = default;
-		inline Term(Term&&) noexcept = default;
-		inline Term& operator=(Term const& other) = default;
-		~Term() = default;
+    public:
+        inline Term(size_t size, BitArray term);
+        inline Term(Term<BitArray> const& first,
+                    Term<BitArray> const& second,
+                    BitArray mask);
+        inline Term(Term const&) = default;
+        inline Term(Term&&) noexcept = default;
+        inline Term& operator=(Term const& other) = default;
+        ~Term() = default;
 
-		inline bool isGreyAdjacent(Term& other);
-		inline Term<BitArray> getGroupedTerm(Term& other);
-		inline bool operator==(Term const& other) const;
-		inline bool operator!=(Term const& other) const;
-		inline bool operator<(Term const& oter) const;
+        inline bool isGreyAdjacent(Term& other);
+        inline Term<BitArray> getGroupedTerm(Term& other);
+        inline bool operator==(Term const& other) const;
+        inline bool operator!=(Term const& other) const;
+        inline bool operator<(Term const& oter) const;
 
-		inline int getSetBitNum() const;
-		inline bool isChecked() const;
-		inline auto const& getMinterms() const;
-		inline auto getEquation() const;
-	};
-
-
-
-	template<typename BitArray>
-	inline Term<BitArray>::Term(size_t size, BitArray term)
-		:_numberOfOne(0),
-		 _checked(true),
-		 _size(size),
-		 _bitArray(term),
-		 _xMask(0),
-		 _result(0)
-	{
-		BitArray end = 1 << size;
-		for(BitArray it = 1; it != end; it <<= 1)
-		{
-			if(_bitArray & it)
-				++_numberOfOne;
-		}
-		_minTerm.push_back(term);
-	}
+        inline int getSetBitNum() const;
+        inline bool isChecked() const;
+        inline auto const& getMinterms() const;
+        inline auto getEquation() const;
+    };
 
 
-	template<typename BitArray>
-	inline Term<BitArray>::Term(Term<BitArray> const& first,
-								Term<BitArray> const& second,
-								BitArray mask)
-		:_numberOfOne(first._numberOfOne),
-		 _checked(true),
-		 _size(first._size),
-		 _bitArray(first._bitArray),
-		 _xMask(mask),
-		 _result(0)
-	{
-		_minTerm.insert(_minTerm.end(),
-						first._minTerm.begin(),
-						first._minTerm.end());
-		_minTerm.insert(_minTerm.end(),
-						second._minTerm.begin(),
-						second._minTerm.end());
-	}
 
-	template<typename BitArray>
-	bool Term<BitArray>::isGreyAdjacent(Term<BitArray> &other)
-	{
-		if(_xMask != other._xMask)
-			return false;
+    template<typename BitArray>
+    inline Term<BitArray>::Term(size_t size, BitArray term)
+        :_numberOfOne(0),
+         _checked(true),
+         _size(size),
+         _bitArray(term),
+         _xMask(0),
+         _result(0)
+    {
+        BitArray end = 1 << size;
+        for(BitArray it = 1; it != end; it <<= 1)
+        {
+            if(_bitArray & it)
+                ++_numberOfOne;
+        }
+        _minTerm.push_back(term);
+    }
 
-		_result = (_bitArray ^ other._bitArray) & ~_xMask; //XOR operation
 
-		return _result && !(_result & (_result - 1));
-	}
+    template<typename BitArray>
+    inline Term<BitArray>::Term(Term<BitArray> const& first,
+                                Term<BitArray> const& second,
+                                BitArray mask)
+        :_numberOfOne(first._numberOfOne),
+         _checked(true),
+         _size(first._size),
+         _bitArray(first._bitArray),
+         _xMask(mask),
+         _result(0)
+    {
+        _minTerm.insert(_minTerm.end(),
+                        first._minTerm.begin(),
+                        first._minTerm.end());
+        _minTerm.insert(_minTerm.end(),
+                        second._minTerm.begin(),
+                        second._minTerm.end());
+    }
 
-	template<typename BitArray>
-	Term<BitArray> Term<BitArray>::getGroupedTerm(Term<BitArray> &other) 
-	{
-		this->_checked = false;
-		other._checked = false;
+    template<typename BitArray>
+    bool Term<BitArray>::isGreyAdjacent(Term<BitArray> &other)
+    {
+        if(_xMask != other._xMask)
+            return false;
 
-		auto newMask = _result | _xMask;
+        _result = (_bitArray ^ other._bitArray) & ~_xMask; //XOR operation
 
-		return Term(*this, other, newMask);
-	}
+        return _result && !(_result & (_result - 1));
+    }
 
-	template<typename BitArray>
-	inline int Term<BitArray>::getSetBitNum() const
-	{
-		return _numberOfOne;
-	}
+    template<typename BitArray>
+    Term<BitArray> Term<BitArray>::getGroupedTerm(Term<BitArray> &other) 
+    {
+        this->_checked = false;
+        other._checked = false;
 
-	template<typename BitArray>
-	inline bool Term<BitArray>::isChecked() const
-	{
-		return _checked;
-	}
+        auto newMask = _result | _xMask;
 
-	template<typename BitArray>
-	bool Term<BitArray>::operator==(Term const& other) const
-	{
-		return _bitArray == other._bitArray
-			&& _xMask == other._xMask
-			&& _checked == other._checked;
-	}
+        return Term(*this, other, newMask);
+    }
 
-	template<typename BitArray>
-	bool Term<BitArray>::operator!=(Term const& other) const
-	{
-		return _bitArray != other._bitArray
-			|| _xMask != other._xMask
-			|| _checked != other._checked;
-	}
+    template<typename BitArray>
+    inline int Term<BitArray>::getSetBitNum() const
+    {
+        return _numberOfOne;
+    }
 
-	template<typename BitArray>
-	bool Term<BitArray>::operator<(Term const& other) const
-	{
-		if(_bitArray == other._bitArray)
-			return _xMask < other._xMask;
-		return _bitArray < other._bitArray;
-	}
+    template<typename BitArray>
+    inline bool Term<BitArray>::isChecked() const
+    {
+        return _checked;
+    }
 
-	template<typename BitArray>
-	auto const& Term<BitArray>::getMinterms() const
-	{
-		return _minTerm;
-	}
+    template<typename BitArray>
+    bool Term<BitArray>::operator==(Term const& other) const
+    {
+        return _bitArray == other._bitArray
+            && _xMask == other._xMask
+            && _checked == other._checked;
+    }
 
-	template<typename BitArray>
-	auto Term<BitArray>::getEquation() const
-	{
-		std::vector<int> equation;
+    template<typename BitArray>
+    bool Term<BitArray>::operator!=(Term const& other) const
+    {
+        return _bitArray != other._bitArray
+            || _xMask != other._xMask
+            || _checked != other._checked;
+    }
 
-		for(auto i = 1; i < static_cast<int>(_size); ++i)
-		{
-			BitArray mask = 1 << (_size - 1);
-			mask = mask >> (i - 1);
+    template<typename BitArray>
+    bool Term<BitArray>::operator<(Term const& other) const
+    {
+        if(_bitArray == other._bitArray)
+            return _xMask < other._xMask;
+        return _bitArray < other._bitArray;
+    }
 
-			if((_xMask & mask) != 0)
-				continue;
+    template<typename BitArray>
+    auto const& Term<BitArray>::getMinterms() const
+    {
+        return _minTerm;
+    }
 
-			if((_bitArray & mask) != 0)
-				equation.push_back(i);
-			else
-			{
-				equation.push_back(-i);
-			}
-		}
-		return equation;
-	}
+    template<typename BitArray>
+    auto Term<BitArray>::getEquation() const
+    {
+        std::vector<int> equation;
+
+        for(auto i = 1; i < static_cast<int>(_size); ++i)
+        {
+            BitArray mask = 1 << (_size - 1);
+            mask = mask >> (i - 1);
+
+            if((_xMask & mask) != 0)
+                continue;
+
+            if((_bitArray & mask) != 0)
+                equation.push_back(i);
+            else
+            {
+                equation.push_back(-i);
+            }
+        }
+        return equation;
+    }
 }
 #endif
